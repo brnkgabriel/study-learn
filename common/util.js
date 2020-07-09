200d4a1b3ddc8e3daa94025522143deff418cca1
brnkgabriel <brnkgabriel@gmail.com>
2020-07-09 06:55:25 +0100

    initialize

---
+++ common/util.js
@@ -0,0 +1,47 @@
+class Tag {
+  constructor(properties) {
+    this.tag          = properties[0]
+    this.attributes   = properties[1]
+    this.styles       = properties[2]
+    this.textContent  = properties[3]
+    this.element      = null
+  }
+
+  get() {
+    return this.create()
+      .setAttributes()
+      .setStyle()
+      .setHTML()
+      .getElement()
+  }
+
+  create() {
+    this.element = document.createElement(this.tag)
+    return this
+  }
+
+  assignAttribute(object) {
+    var keys = Object.keys(object)
+    keys.forEach(key => this.element.setAttribute(key, object[key]) )
+  }
+
+  setAttributes() {
+    this.assignAttribute(this.attributes)
+    return this
+  }
+
+  setStyle() {
+    this.assignAttribute(this.styles)
+    return this
+  }
+
+  setHTML() {
+    this.element.innerHTML = this.textContent
+    return this
+  }
+
+  getElement() { return this.element }
+
+  static appendMany2One(many, one) { many.forEach(each => one.appendChild(each)) }
+
+}
